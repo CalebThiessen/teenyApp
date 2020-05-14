84a9e771a5c4cdde119a6addd9d83ba1f07143ed
CliffGreene <lifeguardtwenty@gmail.com>
2020-05-14 13:40:14 -0600

    adding POST route

--- express_server.js
+++ express_server.js
@@ -2,12 +2,20 @@ const express = require("express")
 const app = express()
 const PORT = 8080;
 app.set("view engine", "ejs")
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
-
+function randomStringGenerator(length, chars) {
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+let rstring = randomStringGenerator(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+console.log(rstring)
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
@@ -21,11 +29,22 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+  
   app.get("/urls/:shortURL", (req, res) => {
     
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
   });
+  
+  
 
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
