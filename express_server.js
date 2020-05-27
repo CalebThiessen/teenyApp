710fe515ea12aab996d56c172a0af07bb0916c71
CliffGreene <lifeguardtwenty@gmail.com>
2020-05-27 15:09:49 -0600

    adding user privacy for url database, ability for only registered users to edit, or delete entries

--- express_server.js
+++ express_server.js
@@ -8,8 +8,8 @@ const cookieParser = require('cookie-parser')
 app.use(cookieParser());
 
 const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "sv9snd": "http://www.google.com"
+    "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "defUser"},
+    "sv9snd": {longURL: "http://www.google.com", userID: "defUser"}
 };
 
 const users = { 
@@ -30,7 +30,18 @@ function randomStringGenerator(length, chars) {
   for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
   return result;
 }
-let rstring = randomStringGenerator(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+function urlsForID(id) {
+  let idURLS = {}
+  for (elem in urlDatabase) {
+      if (urlDatabase[elem].userID === id.cookies.userID) {
+        idURLS[elem] = urlDatabase[elem]
+        
+      }
+      
+    }
+ return idURLS;
+}
 
 app.get("/", (req, res) => {
     res.send("Hello!");
@@ -41,36 +52,49 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  if (req.cookies.userID) {
+    let usersURLS = urlsForID(req);
+    let templateVars = { urls: usersURLS, username: users[req.cookies.userID] };
+    res.render("urls_index", templateVars);
+  } else {
+    
+    res.redirect("/login");
+  }
+});
 
-  let templateVars = { urls: urlDatabase, username: users[req.cookies.userID] };
   
-  
-  res.render("urls_index", templateVars);
-  });
-
-  app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => {
    
     let templateVars = { urls: urlDatabase, username: req.cookies["userID"] };
+   
+    if (templateVars.username){
     res.render("urls_new", templateVars);
+    } else {
+      res.redirect("/login")
+    };
   });
 
   app.post("/urls", (req, res) => {
-    urlDatabase[rstring] = req.body.longURL
-    res.redirect("/urls/" + rstring)        
+    let urlRString = randomStringGenerator(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    urlDatabase[urlRString] = {longURL: req.body.longURL, userID: req.cookies["userID"] }
+    
+    res.redirect("/urls/" + urlRString)        
   });
   
   app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["userID"] };
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, username: req.cookies["userID"] };
     res.render("urls_show", templateVars);
   });
   
   app.get("/u/:shortURL", (req, res) => {
-    const longURL = urlDatabase[req.params.shortURL]
-    res.redirect(longURL);
+   const longURL = urlDatabase[req.params.shortURL].longURL
+   res.redirect(longURL);
   });
   
   app.post("/urls/:shortURL", (req, res) => {
-    urlDatabase[req.params.shortURL] = req.body.newURL
+    if (urlDatabase[req.params.shortURL].userID === req.cookies.userID){
+    urlDatabase[req.params.shortURL].longURL = req.body.newURL
+    }
     res.redirect("/urls")
 
      
@@ -79,7 +103,8 @@ app.get("/urls", (req, res) => {
   });
 
   app.post("/urls/:shortURL/delete", (req, res) => {
-    delete urlDatabase[req.params.shortURL]
+    if (urlDatabase[req.params.shortURL].userID === req.cookies.userID){
+    delete urlDatabase[req.params.shortURL]}
     res.redirect("/urls")
   });
   
@@ -129,7 +154,7 @@ app.get("/registration", (req, res) => {
       console.log("error 403: user with that email already exists!");
       return;
     }
-    const newUserID = rstring;
+    const newUserID = randomStringGenerator(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
     users[newUserID] = {
       id: newUserID,
       email: req.body.email,
@@ -138,7 +163,7 @@ app.get("/registration", (req, res) => {
     res.cookie("userID", newUserID);
     res.redirect("/urls");
 
-    console.log(users);
+    
   });
 
   app.get("/login", (req, res) => {
